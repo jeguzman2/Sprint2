6266c2253b73b4524e4ddde2b41ffb0d904600f2
jeguzman2 <je.guzman2@uniandes.edu.co>
2025-11-06 00:05:00 -0500

    peo

--- provesi/demo/src/main/java/com/provesi/demo/DemoApplication.java
+++ provesi/demo/src/main/java/com/provesi/demo/DemoApplication.java
@@ -2,8 +2,13 @@ package com.provesi.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.provesi.wms.domain")
+@EnableJpaRepositories(basePackages = "com.provesi.wms.repository")
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
