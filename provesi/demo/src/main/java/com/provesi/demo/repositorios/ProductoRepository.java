42a2ac55a5fc38b745aa15173da94987d3c5ef09
jeguzman2 <je.guzman2@uniandes.edu.co>
2025-11-06 01:18:40 -0500

    a

--- provesi/demo/src/main/java/com/provesi/demo/repositorios/ProductoRepository.java
+++ provesi/demo/src/main/java/com/provesi/demo/repositorios/ProductoRepository.java
@@ -1,6 +1,7 @@
 package com.provesi.demo.repositorios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.provesi.demo.model.EstadoProducto;
 import com.provesi.demo.model.Producto;
@@ -8,6 +9,7 @@ import com.provesi.demo.model.Producto;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
   
     Optional<Producto> findByNombre(String nombre);
