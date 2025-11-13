a9d13844dcb89003fb3a51555d902c80ee248c45
jeguzman2 <je.guzman2@uniandes.edu.co>
2025-11-13 13:12:44 -0500

    cambio de ruta

--- provesi/demo/src/main/java/com/provesi/demo/controller/UsuarioController.java
+++ provesi/demo/src/main/java/com/provesi/demo/controller/UsuarioController.java
@@ -92,7 +92,7 @@ public ResponseEntity<List<Usuario>> listarUsuarios(
     return ResponseEntity.ok(usuarios);
 }
 
-@GetMapping("/usuarios/vulnerable/{id}")
+@GetMapping("/vulnerable/{id}")
   // Si quieres, lo puedes proteger con rol admin:
   @PreAuthorize("hasRole('ADMIN')")
   public List<Usuario> probarVulnerable(@PathVariable String id) {
